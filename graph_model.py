231296a231cb3ac9efe8250b245a74e37e07de30
David (Trey) Clark <73671386+Dclark4444@users.noreply.github.com>
2023-04-03 11:06:18 -0500

    Added graph_model and training logs

---
+++ graph_model.py
@@ -0,0 +1,8 @@
+import matplotlib as plt
+
+fig = plt.figure()
+plt.plot(hist.history['loss'], color='teal', label='loss')
+plt.plot(hist.history['val_loss'], color='orange', label='val_loss')
+fig.suptitle('Loss', fontsize=20)
+plt.legend(loc="upper left")
+plt.show()
