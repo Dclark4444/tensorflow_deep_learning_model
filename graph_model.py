8c8cd072e48757e10ec0c94171495cf7a5004ebe
David (Trey) Clark <73671386+Dclark4444@users.noreply.github.com>
2023-04-03 15:03:28 -0500

    Delete graph_model.py

--- graph_model.py
+++
@@ -1,8 +0,0 @@
-import matplotlib as plt
-
-fig = plt.figure()
-plt.plot(hist.history['loss'], color='teal', label='loss')
-plt.plot(hist.history['val_loss'], color='orange', label='val_loss')
-fig.suptitle('Loss', fontsize=20)
-plt.legend(loc="upper left")
-plt.show()
